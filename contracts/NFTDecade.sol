0082bce412f6c2ef258f471ffa7fc4fa91963c0f
KishoreVB70 <kishore07kzr@gmail.com>
2022-10-04 11:09:00 +0530

    made improvements

--- contracts/NFTDecade.sol
+++ contracts/NFTDecade.sol
@@ -3,34 +3,35 @@ pragma solidity ^0.8.4;
 // Import Openzeppline Contract in order to safe contract minting
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-//import openzippline Ownerable to apply Authorization of specific functions
+//import openzeppline Ownerable to apply Authorization of specific functions
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTDecade is ERC721URIStorage,Ownable{
 // define state variables
-    uint256 public nftPrice=0.001 ether; // nft price
-    uint256 public totalSupply; //  total supplay
+    uint256 public nftPrice; // nft price
+    uint256 public supply; //  total supplay
     uint256 public maxSupply; // Max contract supply
     bool public isMintEnabled; // to enable or disable minting
 
     mapping(address =>uint256)public NftMintedToWallet; // track minting to each wallet
 
-    constructor() ERC721("NFT Decade","NFTDEC") {
-        maxSupply=4; // initial contract supply value
+    constructor(uint256 _maxSupply, uint _nftPrice) ERC721("NFT Decade","NFTDEC") {
+        maxSupply= _maxSupply; // initial contract supply value
+        maxSupply= _nftPrice; // initial nft price
 
     }
 
     function toggleMintEnabbled() external onlyOwner {
-        isMintEnabled=!isMintEnabled; //enable minting
+        isMintEnabled = !isMintEnabled; //enable minting
     }
 
-    function setMaxSupply(uint256 maxSupply_) external onlyOwner {
-        maxSupply=maxSupply_; // change contract supply
+    function changeMaxSupply(uint256 maxSupply_) external onlyOwner {
+        maxSupply = maxSupply_; // change contract supply
 
     }
 
-    function setNftPrice(uint256 nftPrice_) external onlyOwner {
-        nftPrice=nftPrice_; //change nft price
+    function changeNftPrice(uint256 nftPrice_) external onlyOwner {
+        nftPrice = nftPrice_; //change nft price
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -38,17 +39,15 @@ contract NFTDecade is ERC721URIStorage,Ownable{
     }
 
     function mintNFT() external payable {
-        require(isMintEnabled , "Mint not enabled"); // check if minitng is enable?
-        require(maxSupply>totalSupply ,"Sold Out"); //check if the bot still have NFT or not
-        require(NftMintedToWallet[msg.sender]<1 , "Minted Before"); //check if the wallet minted before?
-        require(msg.value==nftPrice , "invalid value"); //check the provided value to nft price
+        require(isMintEnabled, "Mint not enabled"); // check if minitng is enable?
+        require(maxSupply > supply ,"Sold Out"); //check if the bot still have NFT or not
+        require(NftMintedToWallet[msg.sender] < 1 , "Minted Before"); //check if the wallet minted before?
+        require(msg.value == nftPrice , "invalid value"); //check the provided value to nft price
         NftMintedToWallet[msg.sender]++; // save the minted nft to user
-        totalSupply++; // increase total supply
-        uint256 tokenId=totalSupply; //define token id
+        uint256 tokenId = supply; //define token id
+        supply++; // increase total supply
         _safeMint(msg.sender, tokenId); //start minting
 
     }
-    
-
 
 }
